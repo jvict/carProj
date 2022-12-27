b187525d2f7498705252c5e51c672f2d2ce5f23a
Joao Victor Tiburcio <jctiburcio@rd.com.br>
2022-12-26 21:54:06 -0300

    init project

---
+++ backend/carProj/carProj/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for carProj project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'carProj.settings')
+
+application = get_wsgi_application()
